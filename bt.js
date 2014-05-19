e3b7e3f1578192066bb9c572b300e61e5886e91c
Nath <nathb2b@gmail.com>
2014-05-19 18:42:02 +0200

    Update bt.js

--- bt.js
+++ bt.js
@@ -26,16 +26,21 @@ var benchTools = {
             gc && gc();
             setTimeout(function () {
                 gc && gc();
-                var keep = [], i, tm = Date.now(), tmp, mem = gc && window.performance.memory.usedJSHeapSize, v;
+                var keep = [], i, tm = Date.now(), tmp, mem = gc && window.performance.memory.usedJSHeapSize, used, cleaned;
 
                 for ( i = 0 ; i < totalTests ; i++ ) {
                     fn(i);
                 }
 
                 moys.A.push(tm = Date.now() - tm);
-                gc && moys.Amem.push(v = Math.round( (window.performance.memory.usedJSHeapSize-mem)/(1024*1024) *100 ) / 100);
+                if (gc){
+                    used = Math.round( (window.performance.memory.usedJSHeapSize-mem)/(1024*1024) *100 ) / 100;
+                    gc();
+                    cleaned = Math.round( (used-window.performance.memory.usedJSHeapSize)/(1024*1024) *100 ) / 100;
+                    moys.Amem.push([used, cleaned]);
+                }
                 //<debug>
-                console.info("Samples "+(++z)+" : " + tm + 'ms, mem delta : '+v+'Mo');//( '+tmp.getFullAddress()+' )
+                console.info("Samples "+(++z)+" : " + tm + 'ms, mem delta : '+(used-cleaned)'Mo; '+cleaned+'Mo taken by garbage collect');//( '+tmp.getFullAddress()+' )
                 //</debug>
             });
         }
@@ -43,15 +48,16 @@ var benchTools = {
 
         setTimeout(function () {
 
-            var mA = 0, mS = 0, memA=0, memS=0;
+            var mA = 0, mS = 0, memA=0, memS=0, memGC=0;
             moys.A.map(function ( v ) {
                 mA += v;
             });
             gc && moys.Amem.map(function ( v ) {
-                memA += v;
+                memA += v[0];
+                memGC += v[1];
             });
             //<debug>
-            console.warn("Bench result : " + (mA / moys.A.length) + "ms / " + (memA / moys.A.length) + "Mo in " + moys.A.length + " records");
+            console.warn("Bench result : " + (mA / moys.A.length) + "ms / " + (memA / moys.A.length) + "Mo in " + moys.A.length + " records\n "+(100*(memA/memGC))+' % garbage collected';
             //</debug>
 
         },500);
