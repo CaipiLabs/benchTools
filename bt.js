7181f2d1b04c59985ec6504982691dfebb03f950
nath <nathb2b@gmail.com>
2014-05-21 16:44:50 +0200

    ^

--- bt.js
+++ bt.js
@@ -11,9 +11,10 @@ var benchTools = {
      * 
      * results are sent to the console
      */
-    goBenchThat : function (fn, totalTests, totalSamples){
+    goBenchThat : function (fn, totalTests, totalSamples, doAfter){
         var i, z=-1,
-            gc = window.gc || 0;
+            gc = window.gc || 0,
+            testedFn;
 
         var moys = {A : [], S : [], Amem : [], Smem : []};
 
@@ -23,24 +24,26 @@ var benchTools = {
         console.warn("Starting "+totalSamples+" sample of "+totalTests+" trys", fn);
         //</debug>
         for ( i = 0 ; i < totalSamples ; i++ ) {
-            
+
+            gc && gc();
             setTimeout(function () {
-                gc && gc();
                 var keep = [], i, tm = Date.now(), tmp, mem = gc && window.performance.memory.usedJSHeapSize, used, cleaned;
 
+                gc && gc();
                 for ( i = 0 ; i < totalTests ; i++ ) {
                     fn(i);
                 }
 
                 moys.A.push(tm = Date.now() - tm);
                 if (gc){
-                    used = Math.round( (window.performance.memory.usedJSHeapSize-mem)/(1024*1024) *100 ) / 100;
+                    used = window.performance.memory.usedJSHeapSize-mem;
                     gc();
-                    cleaned = Math.round( (used-window.performance.memory.usedJSHeapSize)/(1024*1024) *100 ) / 100;
+                    cleaned = Math.round( (used-(window.performance.memory.usedJSHeapSize-mem))/(1024*1024) *100 ) / 100;
+                    used = Math.round( used/(1024*1024) *100 ) / 100;
                     moys.Amem.push([used, cleaned]);
                 }
                 //<debug>
-                console.info("Samples "+(++z)+" : " + tm + 'ms, mem delta : '+(used-cleaned)+'Mo; '+cleaned+'Mo taken by garbage collect');//( '+tmp.getFullAddress()+' )
+                console.info("Samples "+(++z)+" : " + tm + 'ms: mem delta : '+used+'Mo; \nAfter collect : '+(used-cleaned)+'Mo\n '+(100*cleaned/used)+' % cleaned');//( '+tmp.getFullAddress()+' )
                 //</debug>
             });
         }
@@ -48,18 +51,19 @@ var benchTools = {
 
         setTimeout(function () {
 
-            var mA = 0, mS = 0, memA=0, memS=0, memGC=0;
+            var mA = 0, mS = 0, memA=0, memReal=0, memGC=0;
             moys.A.map(function ( v ) {
                 mA += v;
             });
             gc && moys.Amem.map(function ( v ) {
                 memA += v[0];
                 memGC += v[1];
+                memReal += (v[0]-v[1])
             });
             //<debug>
-            console.warn("Bench result : " + (mA / moys.A.length) + "ms / " + (memA / moys.A.length) + "Mo in " + moys.A.length + " records\n "+(100*(memA/memGC))+' % garbage collected');
+            console.warn("Bench average results : \n " + (mA / moys.A.length) + "ms run time,\n" + (memReal / moys.A.length) + "Mo really used memory,\n and " + (memA / moys.A.length) + "Mo used memory before gc() ("+(100*(memGC/memA))+' % garbage collected\n In ' + moys.A.length + " records");
             //</debug>
-
+            doAfter && setTimeout(doAfter,1);
         },500);
     }
 }, bt = benchTools;
